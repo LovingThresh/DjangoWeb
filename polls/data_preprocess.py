95d2cfe08e89457b6b2c93d4daadf9207bb3e860
liuye <csu1704liuye@163.com>
2023-03-05 15:35:31 +0800

    Update

--- polls/data_preprocess.py
+++ polls/data_preprocess.py
@@ -5,7 +5,10 @@
 # @File    : data_preprocess.py
 # @Software: PyCharm
 
+import cv2
 import timm
+import base64
+import numpy as np
 from PIL import Image
 import torchvision.transforms as T
 from timm.data import resolve_data_config
@@ -30,3 +33,25 @@ def data_preprocess_for_timm(img_path):
     img = Image.open(img_path).convert('RGB')
 
     return transform(img).unsqueeze(0).numpy(), image_tfm(img).unsqueeze_(0).numpy()
+
+
+def img2base(image_path):
+    # 读取图像
+    image = cv2.imread(image_path)
+    # 将图像编码成JPEG格式
+    _, buffer = cv2.imencode('.jpg', image)
+    # 将JPEG图像转换为Base64字符串
+    image_base64 = base64.b64encode(buffer).decode('utf-8')
+
+    return image_base64
+
+
+def base2img(image_base64):
+    # 将Base64字符串解码为字节数组
+    image_bytes = base64.b64decode(image_base64)
+    # 将字节数组转换为NumPy数组
+    image_buffer = np.frombuffer(image_bytes, dtype=np.uint8)
+    # 从NumPy数组中解码图像
+    image = cv2.imdecode(image_buffer, cv2.IMREAD_COLOR)
+
+    return image
